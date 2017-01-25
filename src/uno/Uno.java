f30e0ba64295c3bd2d689031f39a32c3f6c8456a
ChillHumanoid <jonat@DESKTOP-VGFBMC4.fios-router.home>
2017-01-25 17:14:27 -0500

    Code cleanup. commented player 1. pretty much follows to AI

--- src/uno/Uno.java
+++ src/uno/Uno.java
@@ -160,6 +160,7 @@ public class Uno {
                     }
                 }
                 checkDraw(deck, discardPile);
+                System.out.println();
                 break;
              
             }
